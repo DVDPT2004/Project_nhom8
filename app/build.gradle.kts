ff00c2781c43630642e1cd559c5cd29eee9f75ca
doanh dao <quangdoanh04072004@gmail.com>
2024-10-11 00:25:05 +0700

    Viet-sua-lan-1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,12 +48,7 @@ dependencies {
     implementation("com.sun.mail:android-activation:1.6.2")
     implementation("com.sun.mail:android-mail:1.6.2")
 //Viet
-    implementation ("androidx.appcompat:appcompat:1.6.1")  // Phiên bản có thể thay đổi
-    implementation ("androidx.core:core-ktx:1.10.1")
-    implementation ("com.google.android.material:material:1.10.0")
-    implementation ("me.relex:circleindicator:2.1.6")
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-    implementation ("com.google.android.material:material:1.9.0")
+
 //Doanh
     implementation("com.github.PhilJay:MPAndroidChart:v3.0.2")
 
