1f1d7223cfd3eacf41a0aed6dd089f464abad30e
doanh dao <quangdoanh04072004@gmail.com>
2024-10-10 23:13:04 +0700

    Ghep_dâatabase_1

--- app/src/main/java/Database/MainData/MainData.java
+++ app/src/main/java/Database/MainData/MainData.java
@@ -16,11 +16,17 @@ private static final int DATABASE_VERSION = 1;
 
 // create table
 //  vd :
-//private static final String CREATE_TABLE1 = "CREATE TABLE IF NOT EXISTS table1 (" +
-//        "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-//        "name TEXT NOT NULL);";
+private static final String CREATE_TABLE_Urer = "CREATE TABLE User (user_id INTEGER PRIMARY KEY AUTOINCREMENT , full_name TEXT , email TEXT UNIQUE , password TEXT , role  TEXT)";
 
+private static final String CREATE_TABLE_SanPham = "CREATE TABLE SanPham (maSanPham INTEGER PRIMARY KEY AUTOINCREMENT, tenSanPham NVARCHAR(100), madanhmuc INT, gia INT, tinhTrang NVARCHAR(20), discount INT, maPhanHoi INT, anhSanPham TEXT, anhMota1 TEXT, anhMota2 TEXT, anhMota3 TEXT, anhMota4 TEXT, moTaSanPham TEXT)";
 
+private static final String CREATE_TABLE_Danhmuc ="CREATE TABLE Danhmuc (maDanhMuc INTEGER PRIMARY KEY AUTOINCREMENT, tenDanhMuc NVARCHAR(50))";
+
+private static final String CREATE_TABLE_DonHang ="CREATE TABLE DonHang (maDonHang INTEGER PRIMARY KEY AUTOINCREMENT, ngayGioDatHang DATETIME, ThanhTien INT, user_id INT, noiGiaoHang TEXT, soDienThoai NVARCHAR(15), phuongThucThanhToan NVARCHAR(20), tenKH NVARCHAR(50))";
+
+private static final String CREATE_TABLE_PhanHoi = "CREATE TABLE PhanHoi (maPhanHoi INT PRIMARY KEY AUTOINCREMENT, user_id INT, thoiGianPhanHoi DATETIME, NoiDung TEXT, media1 VARCHAR(255), media2 VARCHAR(255), media3 VARCHAR(255), soDienThoai VARCHAR(15), tenKH VARCHAR(100), maDonHang INT, FOREIGN KEY (user_id) REFERENCES User(user_id), FOREIGN KEY (maDonHang) REFERENCES DonHang(maDonHang));";
+
+private static final String CREATE_TABLE_chitietDonHang="CREATE TABLE chiTietDonHang (maDonHang INT, maSanPham INT, soLuong INT, PRIMARY KEY (maDonHang, maSanPham))";
     public MainData(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
@@ -38,7 +44,12 @@ private static final int DATABASE_VERSION = 1;
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
           // Thực hiện câu lệnh tạo bảng
-          //  sqLiteDatabase.execSQL(CREATE_TABLE1);
+            sqLiteDatabase.execSQL(CREATE_TABLE_Urer);
+            sqLiteDatabase.execSQL(CREATE_TABLE_SanPham);
+            sqLiteDatabase.execSQL(CREATE_TABLE_Danhmuc);
+            sqLiteDatabase.execSQL(CREATE_TABLE_DonHang);
+            sqLiteDatabase.execSQL(CREATE_TABLE_chitietDonHang);
+            sqLiteDatabase.execSQL(CREATE_TABLE_PhanHoi);
     }
 
     @Override
